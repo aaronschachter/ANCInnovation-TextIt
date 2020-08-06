02251757e29e6871966cef5efcc2f74a958c20a1
Aaron Schachter <aaronschachter@gmail.com>
2020-08-05 18:45:13 -0700

    Cleanup

--- api/middleware/getContacts.js
+++ api/middleware/getContacts.js
@@ -9,9 +9,17 @@ module.exports = function getContacts() {
     try {
       const apiRes = await textIt.getContactsByGroupId('6384e6d4-b42c-4fd6-9cfa-721ec15c538a');
 
-      logger.debug('TextIt response', apiRes.body);
+     
+      const { next, prev, results } = apiRes.body;
 
-      return res.send({ data: apiRes.body });
+      const data = results.map((contact) => {
+        const { uuid, groups, urns } = contact;
+        return { uuid, phone: urns[0], groups };
+      });
+
+      logger.debug('Sending response', data);
+
+      return res.send({ data, meta: { next, prev } });
     } catch (error) {
       return next(error);
     }
