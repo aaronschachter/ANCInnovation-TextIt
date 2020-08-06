3612bb6d405d11b16d64d35bb1a2700b6735bb0a
Aaron Schachter <aaronschachter@gmail.com>
2020-08-05 18:05:45 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const server = require('./server');
+
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => console.log(`Server running on ${port}.`));
---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+module.exports = app;
