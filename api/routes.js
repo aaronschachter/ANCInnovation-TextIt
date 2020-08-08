9d35ff14a7956c38a179fb419d04c22b07241db4
Aaron Schachter <aaronschachter@gmail.com>
2020-08-08 07:46:18 -0700

    Done

--- api/routes.js
+++ api/routes.js
@@ -5,18 +5,18 @@ const logger = require('heroku-logger');
 const authenticateMiddleware = require('./middleware/authenticate');
 const getContactMiddleware = require('./middleware/inbox/getContact');
 const sendToZapierMiddleware = require('./middleware/inbox/sendToZapier');
+const parseFlowEventMiddleware = require('./middleware/inbox/parseFlowEvent');
 const addSubcribersMiddleware = require('./middleware/subscriberGroups/addSubscribers');
 const getSubcribersMiddleware = require('./middleware/subscriberGroups/getSubscribers');
 const createSubscriberGroupsMiddleware = require('./middleware/subscriberGroups/createSubscriberGroups');
-const parseFlowEventMiddleware = require('./middleware/inbox/parseFlowEvent');
 
 /**
  * API routes.
  */
 module.exports = (app) => {
-  app.get('/', (req, res) => res.send('OK'));
-
   app.use(authenticateMiddleware());
+
+  app.get('/', (req, res) => res.send('OK'));
 
   app.post('/api/v1/subscriber-groups',
     createSubscriberGroupsMiddleware(),
